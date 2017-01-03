3875e90a98168629c9c8708a80ea4f103f6e6d49
Nigam Goyal <Nigam Goyal>
2017-01-03 14:30:20 +0530

    index

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-var express = require('express');
-var app = express();
+var http = require("http");
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
 
 
