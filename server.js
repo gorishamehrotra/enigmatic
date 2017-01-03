ef89f884f5cad7041d89099a751bcfea41477582
Nigam Goyal <Nigam Goyal>
2017-01-03 16:58:16 +0530

    add privacy html file 3

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 var express = require('express'),
     bodyParser = require('body-parser'),
     webhook = require('./modules/webhook'),
+    path = require('path'),
     app = express();
 
 app.set('port', process.env.PORT || 5000);
@@ -12,9 +13,8 @@ app.use(bodyParser.json());
 app.get('/webhook', webhook.handleGet);
 app.post('/webhook', webhook.handlePost);
 
-
-app.get('/privacy', function(request, response) {
-  response.render('pages/index');
+app.get('/privacy', function(req, res) {
+    res.sendFile(path.join(__dirname + '/privacy.html'));
 });
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
