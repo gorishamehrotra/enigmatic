afe57f4c49ab2ce3fab1619c80fe678434e617f9
Nigam Goyal <Nigam Goyal>
2017-01-04 18:10:12 +0530

    db connection

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     webhook = require('./modules/webhook'),
     path = require('path'),
-    app = express();
+    pgp = require('pg-promise'),
+    app = express(),
+    db = pgp('postgres://qasytzcgbmpbhj:873dfc28dd02790ee007216f213c5e5a4fec821cec8e39788b402dad5b3eb22d@ec2-184-72-249-88.compute-1.amazonaws.com:5432/dfv2bfstc4a302?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory');
 
 app.set('port', process.env.PORT || 5000);
 
