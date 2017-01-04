63def9de15ff5882b92fff9469ec5b2a3e5f7c30
Nigam Goyal <Nigam Goyal>
2017-01-04 18:14:02 +0530

    connection correction

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     webhook = require('./modules/webhook'),
     path = require('path'),
-    pgp = require('pg-promise'),
+    pgp = require('pg-promise')(),
     app = express(),
     db = pgp('postgres://qasytzcgbmpbhj:873dfc28dd02790ee007216f213c5e5a4fec821cec8e39788b402dad5b3eb22d@ec2-184-72-249-88.compute-1.amazonaws.com:5432/dfv2bfstc4a302?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory');
 
