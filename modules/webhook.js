4cc1a4aace4ed4a87dbc874da821b64c2ec96a15
gorisha <gorisha@concret.io>
2017-01-04 12:27:26 +0530

    add debug logs

--- modules/webhook.js
+++ modules/webhook.js
@@ -94,17 +94,22 @@ let handlePost = (req, res) => {
         
         let sender = event.sender.id;
         if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+            console.log("1");
             sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
         } else if (event.message && event.message.text) {
+            console.log("2");
             processText(event.message.text, sender);
         } else if (event.postback) {
             let payload = event.postback.payload.split(",");
             if (payload[0] === "view_contacts") {
+                console.log("31");
                 sendMessage({text: "OK, looking for your contacts at " + payload[2] + "..."}, sender);
                 salesforce.findContactsByAccount(payload[1]).then(contacts => sendMessage(formatter.formatContacts(contacts), sender));
             } else if (payload[0] === "close_won") {
+                console.log("32");
                 sendMessage({text: `OK, I closed the opportunity "${payload[2]}" as "Close Won". Way to go Christophe!`}, sender);
             } else if (payload[0] === "close_lost") {
+                console.log("33");
                 sendMessage({text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".`}, sender);
             }
         }
